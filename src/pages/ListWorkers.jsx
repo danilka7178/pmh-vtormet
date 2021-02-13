637f58400a1748965a8398933a5d0184383738e8
danilka7178 <danya.sidorow@yandex.ru>
2021-02-13 06:39:04 +0300

    filter rdy, need dsptch to state

--- src/pages/ListWorkers.jsx
+++ src/pages/ListWorkers.jsx
@@ -5,7 +5,16 @@ import { useSelector } from "react-redux";
 
 function ListWorkers() {
 
-   const data = useSelector(state => state.workersListVault.workersList)
+   const data = useSelector(state => state.workersListVault.workersList);
+   const [inputSearchWorkerValue, setInputSearchWorkerValue] = React.useState("");
+
+   const findWorker = () => {
+      console.log(data.filter(({ firstName }) => {
+         return (
+            firstName.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase())
+         )
+      }))
+   }
 
    return (
       <div className="container">
@@ -13,7 +22,22 @@ function ListWorkers() {
             <div className="list-workers__description">Список сотрудников ООО "ПМХ-ВТОРМЕТ"</div>
             <div className="list-workers__side">
                <button className="list-workers__btn">Добавить сотрудника</button>
-               <input className="list-workers__search" type="text" placeholder="Поиск сотрудников" />
+               <div>
+                  <input
+                     className="list-workers__search"
+                     type="text"
+                     placeholder="Введите искомое"
+                     value={inputSearchWorkerValue}
+                     onChange={(e) => setInputSearchWorkerValue(e.target.value)}
+                     onKeyUp={findWorker}
+                  />
+                  <button
+                     className="list-workers__btn list-workers__btn--search"
+                     onClick={findWorker}
+                  >
+                     Поиск
+               </button>
+               </div>
             </div>
             <WorkersTable />
             <div className="list-workers__summary-info">Общее кол-во работников: {data ? data.length : 0}</div>
