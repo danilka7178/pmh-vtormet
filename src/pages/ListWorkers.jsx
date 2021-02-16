57967d47d4164939474c88513ae761b28280cac8
danilka7178 <danya.sidorow@yandex.ru>
2021-02-16 15:55:40 +0300

    serach inpur rdy!

--- src/pages/ListWorkers.jsx
+++ src/pages/ListWorkers.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import WorkersTable from "../components/WorkersTable";
+import axios from "axios";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setData } from "../store/workersList/actions"
 
 function ListWorkers() {
 
    const data = useSelector(state => state.workersListVault.workersList);
    const [inputSearchWorkerValue, setInputSearchWorkerValue] = React.useState("");
+   const dispatch = useDispatch();
 
-   const findWorker = () => {
-      console.log(data.filter(({ firstName }) => {
-         return (
-            firstName.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase())
-         )
-      }))
+   const findWorker = (e) => {
+      console.log(e, e.code)
+      if (inputSearchWorkerValue === "" && (e.type === "click" || e.code === "Enter" || e.code === "Backspace")) {
+         const getData = async () => {
+            const { data } = await axios.get("https://6024f2ad36244d001797b2c7.mockapi.io/Workers");
+            dispatch(setData(data));
+         };
+         getData();
+      }
+      else {
+         const filteredData = data.filter(({
+            firstName,
+            lastName,
+            middleName,
+            personnelNumber,
+            employmentDate,
+            birthdayDate,
+            division }) => {
+            return (
+               firstName.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase()) ||
+               lastName.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase()) ||
+               middleName.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase()) ||
+               personnelNumber.toString().toLocaleLowerCase().includes(inputSearchWorkerValue.toString().toLocaleLowerCase()) ||
+               employmentDate.toString().toLocaleLowerCase().includes(inputSearchWorkerValue.toString().toLocaleLowerCase()) ||
+               birthdayDate.toString().toLocaleLowerCase().includes(inputSearchWorkerValue.toString().toLocaleLowerCase()) ||
+               division.position.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase()) ||
+               division.subDivision.toLocaleLowerCase().includes(inputSearchWorkerValue.toLocaleLowerCase())
+            )
+         })
+         dispatch(setData(filteredData));
+      }
    }
 
    return (
@@ -40,7 +68,10 @@ function ListWorkers() {
                </div>
             </div>
             <WorkersTable />
-            <div className="list-workers__summary-info">Общее кол-во работников: {data ? data.length : 0}</div>
+            <div className="list-workers__summary-info">
+               {inputSearchWorkerValue === "" ? "Общее кол-во работников: " : "Найдены следующие работники: "}
+               {data ? data.length : 0}
+            </div>
          </div>
       </div>
    );
